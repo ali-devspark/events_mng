5289612ec5aab80869c3d3212646cdb304abb6c5
Ali Ahmed <ali.devspark@gmail.com>
2026-05-13 14:36:34 +0300

    Add Header Security for the system URLs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                // Apply these headers to all routes in your application.
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: *.supabase.co; font-src 'self' data:; connect-src 'self' *.supabase.co; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests;",
+                    },
+                    {
+                        key: "X-Frame-Options",
+                        value: "SAMEORIGIN",
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "strict-origin-when-cross-origin",
+                    },
+                    {
+                        key: "Permissions-Policy",
+                        value: "camera=(self), microphone=(), geolocation=(), browsing-topics=()",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
